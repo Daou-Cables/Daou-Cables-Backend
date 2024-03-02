8c1f8c10b6ccd307a27b3291e310afbc3166f39f
Chris Daou <chris.daou@lau.edu>
2024-03-02 20:11:47 +0200

    modified app.js and added main routes file

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express');
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const mongoose = require("mongoose");
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'exp://192.168.16.101:19001/');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
+
+app.use(cors());
+
+//routes
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
