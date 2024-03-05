6bf23cc55c9f067b77cd304cb2b9ca9b5216d95c
Chris Daou <chris.daou@lau.edu>
2024-03-05 10:20:26 +0200

    added a function to delete an admin

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,6 +1,6 @@
 const Admin = require('../models/Admin');
 
-const addAdmin_post = async (req, res) => {
+module.exports.addAdmin_post = async (req, res) => {
     const { email, password, first_name, last_name } = req.body;
     try{
         const isExist = await Admin.findOne({ email });
@@ -16,5 +16,22 @@ const addAdmin_post = async (req, res) => {
     }
     catch(err){
         res.status(400).json({message: 'Server Error!'});
+    }
+};
+
+module.exports.deleteAdmin_post = async (req, res) => {
+    const { email } = req.body;
+    try{
+        const admin = await Admin.findOne({ email });
+        if(admin && admin.level === 2){
+            await admin.delete();
+            res.status(200).json({message: 'Admin deleted!'});
+        }
+        else{
+            res.status(404).json({message: 'Admin not found!'});
+        }
+    }
+    catch(err){
+        res.status(500).json({message: 'Server Error!'});
     }
 };
--- routes/AdminRoute.js
+++ routes/AdminRoute.js
@@ -5,6 +5,7 @@ const { requireAuth, requireLevelOne } = require('../middleware/Middleware');
 
 //Level 1 admin routes
 router.post('/addAdmin', requireAuth, requireLevelOne, adminController.addAdmin_post);
+router.post('/deleteAdmin', requireAuth, requireLevelOne, adminController.deleteAdmin_post);
 
 module.exports = router;
 
