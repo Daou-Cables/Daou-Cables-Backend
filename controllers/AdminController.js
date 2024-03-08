01f5f0d3eddae77966f798d8d4a3971004acde9d
Chris Daou <chris.daou@lau.edu>
2024-03-08 12:09:18 +0200

    added delete functionality to not overload the bucket

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,5 +1,5 @@
 const { s3Client } = require('../config/s3Client');
-const { PutObjectCommand } = require('@aws-sdk/client-s3');
+const { PutObjectCommand, DeleteObjectCommand} = require('@aws-sdk/client-s3');
 const Admin = require('../models/Admin');
 const Contact = require('../models/Contact');
 const Media = require('../models/Media');
@@ -83,24 +83,32 @@ module.exports.getAdmins_get = async (req, res) => {
 };
 
 module.exports.changeBillboard_post = async (req, res) => {
-    try{
+    try {
         const picture = req.files.picture[0].originalname;
         const file = req.files.picture[0].buffer;
-        const params = {
+        const media = await Media.findOne();
+        if (media && media.billboard) {
+            const oldPictureUrl = new URL(media.billboard);
+            const oldPictureKey = decodeURIComponent(oldPictureUrl.pathname.substring(1));
+            const deleteParams = {
+                Bucket: process.env.BUCKET,
+                Key: oldPictureKey,
+            };
+            await s3Client.send(new DeleteObjectCommand(deleteParams));
+        }
+        const uploadParams = {
             Bucket: process.env.BUCKET,
             Key: picture,
             Body: file,
             ACL: 'public-read'
         };
-        const data = await s3Client.send(new PutObjectCommand(params));
-        const pictureUrl = `https://${params.Bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/${encodeURIComponent(params.Key)}`;
-        const media = await Media.findOne();
+        await s3Client.send(new PutObjectCommand(uploadParams));
+        const pictureUrl = `https://${uploadParams.Bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/${encodeURIComponent(uploadParams.Key)}`;
         media.billboard = pictureUrl;
         await media.save();
         res.status(200).json(media.billboard);
-    }
-    catch(err){
-        res.status(500).json({message: 'Server Error!'});
+    } catch (err) {
+        res.status(500).json({ message: 'Server Error!' });
     }
 };
 
@@ -108,15 +116,24 @@ module.exports.changeVideo_post = async (req, res) => {
     try{
         const video = req.files.video[0].originalname;
         const file = req.files.video[0].buffer;
+        const media = await Media.findOne();
+        if(media && media.video){
+            const oldVideoUrl = new URL(media.video);
+            const oldVideoKey = decodeURIComponent(oldVideoUrl.pathname.substring(1));
+            const deleteParams = {
+                Bucket: process.env.BUCKET,
+                Key: oldVideoKey,
+            };
+            await s3Client.send(new DeleteObjectCommand(deleteParams));
+        }
         const params = {
             Bucket: process.env.BUCKET,
             Key: video,
             Body: file,
             ACL: 'public-read'
         };
-        const data = await s3Client.send(new PutObjectCommand(params));
+        await s3Client.send(new PutObjectCommand(params));
         const videoUrl = `https://${params.Bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/${encodeURIComponent(params.Key)}`;
-        const media = await Media.findOne();
         media.video = videoUrl;
         await media.save();
         res.status(200).json(media.video);
@@ -153,5 +170,29 @@ module.exports.addProduct_post = async (req, res) => {
     }
     catch(err){
         res.status(500).json({message: 'Server Error!', err:err});
+    }
+};
+
+module.exports.deleteProduct_post = async (req, res) => {
+    const { ref } = req.body;
+    try{
+        const product = await Product.findOne({ ref });
+        if(product){
+            const pictureUrl = new URL(product.picture);
+            const pictureKey = decodeURIComponent(pictureUrl.pathname.substring(1));
+            const deleteParams = {
+                Bucket: process.env.BUCKET,
+                Key: pictureKey,
+            };
+            await s3Client.send(new DeleteObjectCommand(deleteParams));
+            await product.deleteOne();
+            res.status(200).json({message: 'Product deleted!'});
+        }
+        else{
+            res.status(404).json({message: 'Product not found!'});
+        }
+    }
+    catch(err){
+        res.status(500).json({message: 'Server Error!'});
     }
 };
