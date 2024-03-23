0d6691f055d9547142b3e8276e174e25ed813390
Jean-Paul <jean.paul.bassil@outlook.com>
2024-03-23 23:41:47 +0200

    fixed bug

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const authController = require('../controllers/AuthController');
 const router = Router();
+const { requireAuth } = require('../middleware/Middleware');
 
 router.post('/login', authController.login_post);
 router.post('/auth/refreshToken', authController.refreshToken_post);
-router.post('/logout'. requireAuth, authController.logout_post);
+router.post('/logout', requireAuth, authController.logout_post);
 
 module.exports = router;
